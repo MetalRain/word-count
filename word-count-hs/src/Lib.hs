232a0afb41be59c39c08d636ca6b47d972f903f0
Otto Martikainen <martikainenotto@gmail.com>
2017-11-09 03:38:51 -0800

    Clean up Haskell implementation

--- word-count-hs/src/Lib.hs
+++ word-count-hs/src/Lib.hs
@@ -6,16 +6,13 @@ import Prelude hiding (words)
 import Data.Text.Lazy (Text, words)
 import Data.Map.Lazy (Map, insertWith, toList, fromList)
 import Data.List (sortBy)
+import Data.Tuple (swap)
 
 accWord :: Map Text Integer -> Text -> Map Text Integer
 accWord map word = insertWith (+) word 1 map
 
-swap :: (a,b) -> (b,a)
-swap (a,b) = (b,a)
-
 wordCount :: Text -> [(Integer, Text)]
 wordCount text = sorted where
-  splits  = words text
-  wordMap = foldl accWord (fromList []) splits
+  wordMap = foldl accWord (fromList []) $  words text
   counts  = map swap $ toList wordMap
   sorted  = sortBy (\(a,_) (b,_) -> (flip compare) a b) counts
