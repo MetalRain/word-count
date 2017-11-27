58fa5ba35aebbf0c38f955fefe36fd8ac62c8ee6
Otto Martikainen <martikainenotto@gmail.com>
2017-11-27 07:56:04 -0800

    Sort and slice rust output

--- word-count-rs/src/main.rs
+++ word-count-rs/src/main.rs
@@ -1,6 +1,7 @@
 use std::fs::File;
 use std::io::prelude::*;
 use std::collections::HashMap;
+use std::vec::Vec;
 
 fn main() {
   let filename = "../data/t8.shakespeare.txt";
@@ -8,14 +9,19 @@ fn main() {
   let mut f = File::open(filename).expect("file not found");
   let mut contents = String::new();
   f.read_to_string(&mut contents)
-    .expect("something went wrong reading the file");
+   .expect("something went wrong reading the file");
 
-  let mut word_map = HashMap::new();
+  let mut word_map: HashMap<&str, u64> = HashMap::new();
 
   for word in contents.split_whitespace() {
-    let count = word_map.entry(word).or_insert(0);
+    let count = word_map.entry(word)
+                        .or_insert(0);
     *count += 1;
   }
 
-  println!("{:?}", word_map);
+  let mut counts: Vec<(&&str, &u64)> = word_map.iter()
+                                   .collect();
+  counts.sort_by(|&(_, a_val), &(_, b_val)| b_val.cmp(a_val));
+
+  println!("{:?}", counts[0..100].to_vec());
 }
